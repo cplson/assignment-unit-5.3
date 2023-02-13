f574ebabc04d2d97b1722bb48a57979540875c0b
James Jacobsen <edjcjacobsen@gmail.com>
2023-02-13 17:00:00 -0600

    Added Albums to collection and logged them to console.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,7 +4,13 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 // TESTS //
-console.log(addToCollection('Colter Wall', 'Imaginary Appalachia', 2015));
+console.log(addToCollection( 'Imaginary Appalachia', 'Colter Wall', 2015));
+console.log(addToCollection( '2014 Forest Hills Drive', 'J Cole', 2014));
+console.log(addToCollection('Jar of Flies', 'Alice in Chains', 1994));
+console.log(addToCollection('MTV unplugged', 'Alice in chains', 1996));
+console.log(addToCollection('Songs of the Plains', 'Colter Wall'));
+console.log(addToCollection('Texas Sun', 'Khruangbin and Leon Bridges', 2020));
+console.log(collection);
 // END TESTS //
 
 // ******************************* //
