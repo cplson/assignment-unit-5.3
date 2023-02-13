2ea3caaebf532e919d4aa5c0c93f8706afac2707
James Jacobsen <edjcjacobsen@gmail.com>
2023-02-13 11:59:57 -0600

    Added collection and addToCollection definition

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,24 @@
 console.log('***** Music Collection *****')
+
+// Global Variables
+let collection = [];
+
+// TESTS //
+console.log(addToCollection('Colter Wall', 'Imaginary Appalachia', 2015));
+// END TESTS //
+
+// ******************************* //
+
+//FUNCTION DEFINITIONS
+
+// Creates a new album and adds it to the collection array
+function addToCollection(title, artist, yearPublished){
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(album);
+    return album;
+}
+//END FUNCTION DEFINITIONS
