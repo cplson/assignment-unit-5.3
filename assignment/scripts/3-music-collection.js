71f193d993c5a61a1809bd87d036564ea267ff05
James Jacobsen <edjcjacobsen@gmail.com>
2023-02-13 17:07:58 -0600

    Added and tested showCollection definition

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,9 +8,10 @@ console.log(addToCollection( 'Imaginary Appalachia', 'Colter Wall', 2015));
 console.log(addToCollection( '2014 Forest Hills Drive', 'J Cole', 2014));
 console.log(addToCollection('Jar of Flies', 'Alice in Chains', 1994));
 console.log(addToCollection('MTV unplugged', 'Alice in chains', 1996));
-console.log(addToCollection('Songs of the Plains', 'Colter Wall'));
+console.log(addToCollection('Songs of the Plains', 'Colter Wall', 2018));
 console.log(addToCollection('Texas Sun', 'Khruangbin and Leon Bridges', 2020));
 console.log(collection);
+showCollection(collection);
 // END TESTS //
 
 // ******************************* //
@@ -26,5 +27,12 @@ function addToCollection(title, artist, yearPublished){
     }
     collection.push(album);
     return album;
+}
+
+function showCollection(array){
+    console.log(`The number of albums in the collection is: ${array.length}`);
+    for(let album of array){
+        console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+    }
 }
 //END FUNCTION DEFINITIONS
