76eca531794815eb61381fe7775f5a46910bd081
James Jacobsen <edjcjacobsen@gmail.com>
2023-02-17 16:32:15 -0600

    Added tracks parameter to addToCollection function and added tracks to all collection albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,16 +2,100 @@ console.log('***** Music Collection *****')
 
 // Global Variables
 let collection = [];
+// let albumTracks = [
+//     {
+//         name: 'Sleeping on the Blacktop',
+//         duration: '3:13'
+//     },
+//     {
+//         name: 'The Devil Wears a Suit and Tie', 
+//         duration: '3:56'
+//     },
+//     {
+//         name: 'Nothin',
+//         duration: '3:23'
+//     }
 
+// ]
 // TESTS //
 
 // Populates collection using addToCollection
-console.log(addToCollection( 'Imaginary Appalachia', 'Colter Wall', 2015, collection));
-console.log(addToCollection( '2014 Forest Hills Drive', 'J Cole', 2014, collection));
-console.log(addToCollection('Jar of Flies', 'Alice in Chains', 1994, collection));
-console.log(addToCollection('MTV unplugged', 'Alice in chains', 1996, collection));
-console.log(addToCollection('Songs of the Plains', 'Colter Wall', 2018, collection));
-console.log(addToCollection('Texas Sun', 'Khruangbin and Leon Bridges', 2020, collection));
+console.log(addToCollection( 'Imaginary Appalachia', 'Colter Wall', 2015, collection, [
+    {
+        name: 'Sleeping on the Blacktop',
+        duration: '3:13'
+    },
+    {
+        name: 'The Devil Wears a Suit and Tie', 
+        duration: '3:56'
+    },
+    {
+        name: 'Nothin',
+        duration: '3:23'
+    }
+]));
+console.log(addToCollection( '2014 Forest Hills Drive', 'J Cole', 2014, collection, [
+    {
+        name: 'Hello',
+        duration: '3:39'
+    },
+    {
+        name: 'Love Yourz', 
+        duration: '3:31'
+    },
+    {
+        name: 'No Role Modelz',
+        duration: '4:52'
+    }
+]));
+console.log(addToCollection('Jar of Flies', 'Alice in Chains', 1994, collection, [
+    {
+        name: 'Nutshell',
+        duration: '4:19'
+    },
+    {
+        name: 'I Stay Away', 
+        duration: '4:14'
+    },
+    {
+        name: "Don't Follow",
+        duration: '4:22'
+    }
+]));
+console.log(addToCollection('MTV unplugged', 'Alice in chains', 1996, collection, [
+    {
+        name: 'Rooster',
+        duration: '6:41'
+    },
+    {
+        name: 'Nutshell', 
+        duration: '4:57'
+    },
+    {
+        name: 'Heaven Beside You',
+        duration: '5:38'
+    }
+]));
+console.log(addToCollection('Songs of the Plains', 'Colter Wall', 2018, collection, [
+    {
+        name: 'Saskatchewan in 1881',
+        duration: '2:52'
+    },
+    {
+        name: 'Manitoba Man', 
+        duration: '3:59'
+    },
+    {
+        name: 'The Trains Are Gone',
+        duration: '2:08'
+    }
+]));
+console.log(addToCollection('Texas Sun', 'Khruangbin and Leon Bridges', 2020, collection, [
+    {
+        name: 'Texas Sun',
+        duration: '4:13'
+    }
+]));
 console.log(collection);
 
 // showCollection
@@ -47,13 +131,15 @@ console.log('Expecting the full collection:', search(collection));
 // FUNCTION DEFINITIONS //
 
 // Creates a new album and adds it to a collection array
-function addToCollection(title, artist, yearPublished, array){
+function addToCollection(title, artist, yearPublished, array, tracks){
     let album = {
         title: title,
         artist: artist,
-        yearPublished: yearPublished
+        yearPublished: yearPublished,
+        tracks: tracks
     }
     array.push(album);
+    
     return album;
 }
 
@@ -62,6 +148,9 @@ function showCollection(array){
     console.log(`The number of albums in the collection is: ${array.length}`);
     for(let album of array){
         console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+        for(let song in array.tracks){
+            console.log(`${song + 1}. ${array.tracks[0]}: ${array.tracks[1]}`);
+        }
     }
 }
 
