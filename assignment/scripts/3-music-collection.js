afbd8ef6594bbc406d054696e0561cf7d5aa36ee
James Jacobsen <edjcjacobsen@gmail.com>
2023-02-17 20:40:15 -0600

    Added tracks search criteria functionality

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,25 +2,11 @@ console.log('***** Music Collection *****')
 
 // Global Variables
 let collection = [];
-// let albumTracks = [
-//     {
-//         name: 'Sleeping on the Blacktop',
-//         duration: '3:13'
-//     },
-//     {
-//         name: 'The Devil Wears a Suit and Tie', 
-//         duration: '3:56'
-//     },
-//     {
-//         name: 'Nothin',
-//         duration: '3:23'
-//     }
 
-// ]
-// TESTS //
+// SETUP //
 
 // Populates collection using addToCollection
-console.log(addToCollection( 'Imaginary Appalachia', 'Colter Wall', 2015, collection, [
+addToCollection( 'Imaginary Appalachia', 'Colter Wall', 2015, collection, [
     {
         name: 'Sleeping on the Blacktop',
         duration: '3:13'
@@ -33,8 +19,8 @@ console.log(addToCollection( 'Imaginary Appalachia', 'Colter Wall', 2015, collec
         name: 'Nothin',
         duration: '3:23'
     }
-]));
-console.log(addToCollection( '2014 Forest Hills Drive', 'J Cole', 2014, collection, [
+]);
+addToCollection( '2014 Forest Hills Drive', 'J Cole', 2014, collection, [
     {
         name: 'Hello',
         duration: '3:39'
@@ -47,8 +33,8 @@ console.log(addToCollection( '2014 Forest Hills Drive', 'J Cole', 2014, collecti
         name: 'No Role Modelz',
         duration: '4:52'
     }
-]));
-console.log(addToCollection('Jar of Flies', 'Alice in Chains', 1994, collection, [
+]);
+addToCollection('Jar of Flies', 'Alice in Chains', 1994, collection, [
     {
         name: 'Nutshell',
         duration: '4:19'
@@ -61,8 +47,8 @@ console.log(addToCollection('Jar of Flies', 'Alice in Chains', 1994, collection,
         name: "Don't Follow",
         duration: '4:22'
     }
-]));
-console.log(addToCollection('MTV unplugged', 'Alice in chains', 1996, collection, [
+]);
+addToCollection('MTV unplugged', 'Alice in chains', 1996, collection, [
     {
         name: 'Rooster',
         duration: '6:41'
@@ -75,8 +61,8 @@ console.log(addToCollection('MTV unplugged', 'Alice in chains', 1996, collection
         name: 'Heaven Beside You',
         duration: '5:38'
     }
-]));
-console.log(addToCollection('Songs of the Plains', 'Colter Wall', 2018, collection, [
+]);
+addToCollection('Songs of the Plains', 'Colter Wall', 2018, collection, [
     {
         name: 'Saskatchewan in 1881',
         duration: '2:52'
@@ -89,26 +75,43 @@ console.log(addToCollection('Songs of the Plains', 'Colter Wall', 2018, collecti
         name: 'The Trains Are Gone',
         duration: '2:08'
     }
-]));
-console.log(addToCollection('Texas Sun', 'Khruangbin and Leon Bridges', 2020, collection, [
+]);
+addToCollection('Texas Sun', 'Khruangbin and Leon Bridges', 2020, collection, [
     {
         name: 'Texas Sun',
         duration: '4:13'
     }
-]));
+]);
+// END SETUP //
+
+// ************************** //
+
+// TESTS //
+
+// addToCollection
+console.log('\n\n');
+console.log('-TEST: addToCollection-')
 console.log(collection);
+console.log('\n\n');
 
 // showCollection
+console.log('-TEST: showCollection-');
 showCollection(collection);
+console.log('-END TEST: showCollection-');
+console.log('\n\n');
+
 
 // findByArtist: one result
+console.log('-TEST: findByArtist-');
 console.log('Expected one result:', findByArtist('J Cole', collection));
 // findByArtist: no results
 console.log('Expected no results:', findByArtist('Nickelback', collection));
 // findByArtist: two results
 console.log('Expected two results:', findByArtist('Colter Wall', collection));
+console.log('\n\n');
 
 // search - No Results
+console.log('-TEST: search-');
 console.log('Expecting an empty array:', search(collection, { artist: 'Ray Charles', yearPublished: 1957 }));
 // search - One result
 addToCollection('Ray Charles', 'Ray Charles', 1957, collection);
@@ -122,8 +125,15 @@ console.log('Expecting two albums:', search(collection, { artist: 'Ray Charles',
 // search - Expected full collection list
 console.log('Expecting the full collection:', search(collection, {}));
 console.log('Expecting the full collection:', search(collection));
-
-
+// search by track - Expected one result with one track
+console.log('\nExpecting one album with Nothin track:')
+showCollection(search(collection, {tracks: {name: 'Nothin'}}));
+// search by track - Expected two results with one track
+console.log('\nExpecting two albums with Nutshell track:')
+showCollection(search(collection, {tracks: {name: 'Nutshell'}}));
+// search by track - Expected entire collection array
+console.log('\nExpecting entire collection:')
+showCollection(search(collection));
 // END TESTS //
 
 // ******************************* //
@@ -147,10 +157,17 @@ function addToCollection(title, artist, yearPublished, array, tracks){
 function showCollection(array){
     console.log(`The number of albums in the collection is: ${array.length}`);
     for(let album of array){
-        console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
-        for(let song in array.tracks){
-            console.log(`${song + 1}. ${array.tracks[0]}: ${array.tracks[1]}`);
+        console.log(`*${album.title} by ${album.artist}, published in ${album.yearPublished}*`);
+        let i = 1;
+
+        
+        if(album.tracks != undefined){
+            for(let song of album.tracks){
+                console.log(`\t${i}. ${song.name}: ${song.duration}`);
+                i++;
+            }
         }
+        
     }
 }
 
@@ -173,14 +190,24 @@ function search(array, criteria){
     //if no criteria argument was used, set criteria to an empty object
     if(criteria === undefined){
         criteria = {};
-    }
-    for(let album of array){
+    }    
+    for(let album of array){     
         // Finds any exact matches to the criteria parameter and
         // disregards any criteria not provided in the search criteria argument object
         if((criteria.title === undefined || criteria.title === album.title) &&
             (criteria.artist === undefined || criteria.artist === album.artist) &&
             (criteria.yearPublished === undefined || criteria.yearPublished === album.yearPublished)){
-                matches.push(album);
+                if(criteria.tracks === undefined){
+                    matches.push(album);
+                }
+                //This executes if criteria is tracks data
+                else if(album.tracks != undefined){
+                    for(let track of album.tracks){
+                        if(track.name === criteria.tracks.name){
+                            addToCollection(album.title, album.artist, album.yearPublished, matches, [track]);
+                        }
+                    }
+                }
             }
     }
     return matches;
